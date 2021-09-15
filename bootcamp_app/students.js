48e45ae2750ab3b14ec81f017661226a712fd4d4
Tapan Siwach <siwach.tapan@gmail.com>
2021-09-15 17:27:29 -0400

    pass values separately instead of directly into template string

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,14 +8,16 @@ const pool = new Pool({
 });
 
 const args = process.argv.slice(2);
+const cohortName = `%${args[0]}%`;
+const limit = args[1] || 5;
 
 pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '${args[0]}%'
-LIMIT ${args[1] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, [cohortName, limit])
   .then(res => {
     // console.log(res.rows);
     // console.table(res.rows);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,6 +8,7 @@ const pool = new Pool({
 });
 
 const args = process.argv.slice(2);
+console.log(args[0], typeof args[0]);
 
 pool.query(`
 SELECT DISTINCT teachers.name as teacher,
@@ -16,9 +17,9 @@ FROM assistance_requests
   JOIN teachers ON teachers.id = teacher_id
   JOIN students ON students.id = student_id
   JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${args[0] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teachers.name;
-`)
+`, args)
   .then(res => {
     // console.log(res.rows);
     // console.table(res.rows);
