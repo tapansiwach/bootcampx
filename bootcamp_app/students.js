5425bad12f50ff0dd716bf0c7177b2f895c76bf9
Tapan Siwach <siwach.tapan@gmail.com>
2021-09-15 15:09:49 -0400

    modify query to take user input before fetching student details

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,13 +7,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const args = process.argv.slice(2);
+
 pool.query(`
-SELECT id, name, cohort_id
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
-LIMIT 5;
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '${args[0]}%'
+LIMIT ${args[1] || 5};
 `)
   .then(res => {
-    console.log(res.rows);
-    console.table(res.rows);
-  })
-  .catch(err => console.error('query error', err.stack));
+    // console.log(res.rows);
+    // console.table(res.rows);
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    })
+  });
